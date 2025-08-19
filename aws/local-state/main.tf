1f6cbf6b009a69a28c0e238b3baa732115141a41
Arslan Ali <122626416+imarslanalii@users.noreply.github.com>
2025-08-19 05:38:55 -0700

    Update main.tf

--- aws/local-state/main.tf
+++ aws/local-state/main.tf
@@ -1,1 +1,33 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.16"
+    }
+  }
 
+  required_version = ">= 1.2.0"
+}
+
+provider "aws" {
+  region  = "ap-southeast-2"
+}
+
+data "aws_ami" "amazon_linux" {
+  most_recent = true
+  owners      = ["amazon"]
+
+  filter {
+    name   = "name"
+    values = ["amzn2-ami-hvm-*-x86_64-gp2"]
+  }
+}
+
+resource "aws_instance" "app_server" {
+  ami           = data.aws_ami.amazon_linux.id
+  instance_type = "t2.micro"
+
+  tags = {
+    Name = "Terraform_Demo"
+  }
+}
